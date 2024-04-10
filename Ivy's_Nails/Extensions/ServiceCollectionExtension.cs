ee4fff7eaa0ada58ebc31f3dc07aa7aada8b8808
Venelin Krastev <venelinkrstev@gmail.com>
2024-04-10 20:26:36 +0300

    ServiceCollectionExtension added

---
+++ Ivy's_Nails/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using IvysNails.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<IvyNailsDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<IvyNailsDbContext>();
+
+            return services;
+        }
+    }
+}
