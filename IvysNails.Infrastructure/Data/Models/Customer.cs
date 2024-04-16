8252ffbb2a75ad67fc38ff1a7335701a551bf948
Venelin Krastev <venelinkrstev@gmail.com>
2024-04-16 17:56:02 +0300

    add agin Customer table

---
+++ IvysNails.Infrastructure/Data/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IvysNails.Data.DataConstants;
+
+
+namespace IvysNails.Infrastructure.Data.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(CustomerNameMaxLength)]
+        public string CustomerName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(CustomerPhoneNumberMaxLength)]
+        public string CustomerPhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(CustomerEmailMaxLength)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
